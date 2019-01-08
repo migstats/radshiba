7c702a07fc1dd1c7c91bd8ff064a91c5a3fdfd5f
miguel.naranjo <miguel.naranjo@mediatonic.co.uk>
2019-01-08 11:16:05 +0000

    density plot function now documented and ready to be used

--- R/plots.R
+++ R/plots.R
@@ -1,3 +1,9 @@
+#' Simple density plot function
+#'
+#' @param column Name of the column
+#' @param alpha Alpha parameter for ggplot
+#'
+#' @export
 dens <- function(column, alpha = NA){
   ggplot2::ggplot(data.frame(a = column), aes(x = a)) +
     ggplot2::geom_density(alpha = alpha) +

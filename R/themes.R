63dfed5801087fce466f0d7e0c85cf81fcd79d12
miguel.naranjo <miguel.naranjo@mediatonic.co.uk>
2019-02-14 10:00:16 +0000

    correct legend in the theme

--- R/themes.R
+++ R/themes.R
@@ -13,6 +13,9 @@ theme_mt <- function(base_size = 18){
       axis.title.x = element_text(family = "Lucida Sans Unicode", size = 12, vjust = -0.5),
       axis.title.y = element_text(family = "Lucida Sans Unicode", size = 12, vjust = 4, angle = 90),
       plot.caption = element_text(family = "Bebas Neue", size = 12, hjust = 1, vjust = -1),
-      plot.margin = margin(15,15,15,15)
+      plot.margin = margin(15,15,15,15),
+      legend.title = element_text(family = "Bebas Neue", size = 14, hjust = 1, vjust = -1),
+      legend.text = element_text(family = "Lucida Sans Unicode", size = 10),
+      legend.margin = margin(l = -0.5, unit = "cm")
     )
 }

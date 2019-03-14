ac31fcc478c358ce6b9eda4b656ec7ece32c28d2
miguel.naranjo <miguel.naranjo@mediatonic.co.uk>
2019-03-14 15:31:33 +0000

    set generic df columns names

--- R/set-env-var.R
+++ R/set-env-var.R
@@ -5,7 +5,8 @@
 #' @return An enviroment
 #' @export
 df_to_enviroment <- function(data){
+  df_names <- names(data)
   data %>%
-    tidyr::spread(var, val) %>%
+    tidyr::spread(df_names[1], df_names[2]) %>%
     list2env()
 }
